9ddbbbace54a59f9bf0f60c6b57ec2543dd4c15f
looky26 <vajearvin03@gmail.com>
2023-11-01 06:28:21 +0800

    created platinum gold silver exhibition page

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#234C81] py-5 px-5 flex justify-between items-center">
+        <Link href={'/'}>
+        <Image
+        src={"/assets/images/logo.png"}
+        alt="logo"
+        width={120}
+        height={40}
+      />
+        </Link>
+  
+      <div>
+        <ul className="lg:flex hidden text-white text-base space-x-10">
+            <li>Speakers</li>
+            <li>Abstract</li>
+            <Link href={'/sponsorship'}>Sponsors</Link>
+            <li>Explore Cebu</li>
+            <li>About</li>
+        </ul>
+      </div>
+      <button className="text-[#234C81] font-semibold bg-[#FBCE3A] px-5 py-4 rounded-full">Register</button>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/components/SponsorShipBanner.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Nunito_Sans } from "next/font/google";
+
+const nonitosans = Nunito_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const SponsorShipBanner = () => {
+  return (
+    <div className={nonitosans.className}>
+      <div className="bg-[#F2F1E9]">
+        <div className="max-w-7xl mx-auto bg-[#F2F1E9] px-10  md:px-[104px] lg:px-0">
+          <h1 className="text-5xl text-[#234C81] font-semibold pt-[80px] ">
+            Sponsorship / Exhibition
+          </h1>
+          <p className="leading-loose my-10 text-justify">
+            Pharmaceutical companies, corporations and other service firms are
+            invited to promote their brand by obtaining a specially designed
+            Platinum, Gold and Silver Partnership Package. PAPRAS will
+            collaborate with corporate sponsors to customize distinctive
+            sponsorship opportunities that are aimed to optimize each sponsor's
+            specific marketing, <br /> sales and branding objectives.
+          </p>
+          <button className="bg-[#FBCE3A] px-5 py-4 rounded-full mb-[50px] font-semibold">
+            CONTACT US
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SponsorShipBanner;
