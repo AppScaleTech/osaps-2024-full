0557f5e33814031d29c783f26bab8d4971357046
looky26 <vajearvin03@gmail.com>
2023-11-16 11:36:49 +0800

    created homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
+  const commitees = [1, 2, 3, 4, 5, 6];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="bg-[#C4D9DF]">
+        <div className="relative h-[650px] max-w-7xl mx-auto bg-[#C4D9DF] lg:bg-[url('/assets/images/home/bannerimage.png')] bg-no-repeat bg-cover px-10">
+          <div className="pt-20 flex justify-center lg:justify-normal px-10">
+            <img src="/assets/images/home/harmony.png" alt="" />
+          </div>
+          <div className="flex flex-col mt-20 lg:mt-0 lg:absolute top-[350px] ml-0 lg:ml-20 ">
+            <h3 className="text-center text-[22px] text-[#234C81] font-bold">
+              July 4-6, 2024{" "}
+            </h3>
+            <h3 className="text-center text-[22px] text-[#234C81] font-bold">
+              Shangri-la Mactan, Cebu , Philippines
+            </h3>
+            <h3 className="text-center text-[20px] text-[#234C81] mt-14 ">
+              Early bird rate ends on 12/31/2023
+            </h3>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 my-20 gap-y-10">
+        <div>
+          <h1 className="text-3xl lg:text-5xl text-[#234C81] font-bold text-center px-10 lg:pl-20">
+            Be part of the global exchange of knowledge shaping oriental beauty
+            in the world of aesthetics!
+          </h1>
+        </div>
+
+        <div className="flex flex-col justify-center items-center text-lg lg:text-[24px] ">
+          <div className="flex space-x-5 items-center ">
+            <img src="/assets/images/home/img1.png" alt="" />
+            <div className="leading-[1.2]">
+              <p>18th International Congress</p>
+              <p className="font-bold">
+                Oriental Society of <br /> Aesthetic Plastic Surgeons
+              </p>
+            </div>
+          </div>
+          <div className="flex space-x-5 items-center text-lg lg:text-[24px] mt-14">
+            <img
+              className="-mx-[22px]"
+              src="/assets/images/home/img2.png"
+              alt=""
+            />
+            <div className="leading-[1.2]">
+              <p>18th International Congress</p>
+              <p className="font-bold">
+                Oriental Society of <br /> Aesthetic Plastic Surgeons
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="bg-[#F2F1E9]">
+        <div className="max-w-7xl mx-auto py-20">
+          <h1 className="text-3xl lg:text-5xl text-[#234C81] font-bold text-center px-10">
+            Gain insights from global aesthetic experts
+          </h1>
+          <div className="max-w-5xl mx-auto px-10 grid grid-cols-2 lg:grid-cols-3 gap-20 mt-20 ">
+            {commitees.map((item) => (
+              <div key={item}>
+                <div className="w-full flex flex-col justify-center text-center">
+                  <img
+                    className="w-[140px] object-contain mx-auto"
+                    src="/assets/images/commiteepic.png"
+                    alt=""
+                  />
+                  <p className="font-bold">Dr. Nyla Tobias</p>
+                  <p>
+                    Clinical Care Specialist, <br /> Bio Med Science Inc.
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="bg-[url('/assets/images/home/bgimg1.png')] bg-cover bg-no-repeat">
+        <div className="max-w-7xl mx-auto px-4 lg:px-20 py-20">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+            <div className="text-white text-center lg:text-start">
+              <h1 className="text-3xl lg:text-5xl">
+                INDULGE YOURSELF IN A LUXURIOUS LEARNING
+              </h1>
+              <p className="mt-5">
+                Experience the epitome of luxurious learning at OSAPS 2024 Cebu,
+                hosted in the stunning Shangri-La Mactan Cebu. This prestigious
+                event offers attendees an upscale and comfortable educational
+                experience. Immerse yourself in a setting where knowledge and
+                opulence converge, making OSAPS 2024 an unforgettable journey
+                into the world of aesthetic surgery.
+              </p>
+              <div className="flex flex-col lg:flex-row my-10 space-y-2 lg:space-y-0 lg:space-x-2">
+                <button className="bg-[#FBCE3A] rounded-full px-4 py-2 text-[#234C81]">
+                  REGISTER NOW
+                </button>
+                <button className="border border-[#FBCE3A] px-4 py-2 rounded-full text-[#FBCE3A]">
+                  EXPLORE CEBU
+                </button>
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <img
+                src="/assets/images/home/img3.png"
+                alt=""
+                className="max-w-full"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
